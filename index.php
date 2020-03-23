5802ee21458df7cb8c7d5217f1bfb3a2b4a4f6d7
Iakovos Lampadakis <iakovoslampadakis@gmail.com>
2020-03-23 14:30:38 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link rel = "stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel = "stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <link rel = "stylesheet" type="text/css" href="vendors/css/normalize.css">
+    <title>Restaurant</title>
+</head>
+<body>
+    <h1> Hello World! </h1>
+
+</body>
+</html>
